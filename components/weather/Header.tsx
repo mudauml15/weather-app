1b241a936618e58dc17d3f87b48e60fa25eab1f1
Murendeni Mudau <43264524+mudauml15@users.noreply.github.com>
2025-02-05 14:56:30 +0200

    complete project

--- components/weather/Header.tsx
+++ components/weather/Header.tsx
@@ -16,31 +16,32 @@ export function Header({
   setUnit,
 }: HeaderProps) {
   return (
-    <div className="flex items-center justify-between mb-8 bg-[#1E1E1E] mt-10 mx-5">
-      <div className="flex items-center flex-col w-60">
-        <div className="flex">
-          <img src="/Group48.png" className="" alt="weather" />
-          <div className="text-5xl mt-6">WeatherMe </div>
-        </div>
+    <div className="w-full">
+      <img src="/Group48.png" className="" alt="weather" />
 
-        <div className="text-2xl text-white text-right w-60">21:00 pm</div>
+      <div className="flex items-center justify-between mb-8 mt-[-2rem] bg-[#1E1E1E] mx-20">
+        <div className="flex items-center flex-col w-60">
+          <div className="text-5xl mt-6">WeatherMe </div>
+
+          <div className="text-2xl text-white text-right w-60">21:00 pm</div>
+        </div>
+        <nav className="flex gap-8 text-2xl ">
+          {["today", "tomorrow", "monthly"].map((view) => (
+            <button
+              key={view}
+              onClick={() => setActiveView(view)}
+              className={`pb-1 capitalize ${
+                activeView === view
+                  ? "border-b-2 border-white font-bold"
+                  : "text-zinc-400 font-normal "
+              }`}
+            >
+              {view === "monthly" ? "Monthly Forecast" : view}
+            </button>
+          ))}
+        </nav>
+        <div className="flex items-center gap-2"></div>
       </div>
-      <nav className="flex gap-8 text-2xl ">
-        {["today", "tomorrow", "monthly"].map((view) => (
-          <button
-            key={view}
-            onClick={() => setActiveView(view)}
-            className={`pb-1 capitalize ${
-              activeView === view
-                ? "border-b-2 border-white font-bold"
-                : "text-zinc-400 font-normal "
-            }`}
-          >
-            {view === "monthly" ? "Monthly Forecast" : view}
-          </button>
-        ))}
-      </nav>
-      <div className="flex items-center gap-2"></div>
     </div>
   );
 }
